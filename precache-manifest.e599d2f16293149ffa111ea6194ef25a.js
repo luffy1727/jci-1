dfe379236fc1c00245111b94aed44a73a982725c
luffy1727 <tushig.tushig@gmail.com>
2020-02-18 19:29:24 +0800

    Updates

---
+++ precache-manifest.e599d2f16293149ffa111ea6194ef25a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def3996c4d5aad66a9694ffa070044ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "186677d81f8193803038",
+    "url": "/static/css/main.eae16e1f.chunk.css"
+  },
+  {
+    "revision": "f3b838d7d1dfa2ecf29c",
+    "url": "/static/js/2.d9b5f6a2.chunk.js"
+  },
+  {
+    "revision": "186677d81f8193803038",
+    "url": "/static/js/main.0cd798ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b63dc9e7488ee1a2fe7895fd81b41f20",
+    "url": "/static/media/bg_large.b63dc9e7.jpg"
+  },
+  {
+    "revision": "a0ed47f53846b2282e1f9a0c35651c4c",
+    "url": "/static/media/bg_medium.a0ed47f5.jpg"
+  },
+  {
+    "revision": "9b81f74da917a38117191a0113a5700a",
+    "url": "/static/media/buga1.9b81f74d.JPG"
+  },
+  {
+    "revision": "0c232f588b23b30290e3c189554293d5",
+    "url": "/static/media/buga2.0c232f58.JPG"
+  },
+  {
+    "revision": "9f82c0970dcb1f79db8e34de15511f5d",
+    "url": "/static/media/landing-page.9f82c097.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
